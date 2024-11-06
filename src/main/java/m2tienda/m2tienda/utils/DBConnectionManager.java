1b03fcbc56bb62f53ae6a3b0cee9223a207b11b8
RenzoJimenezB <adrianrjb@outlook.es>
2024-11-06 13:57:01 -0500

    DBConnectionManager class

--- src/main/java/m2tienda/m2tienda/utils/DBConnectionManager.java
+++ src/main/java/m2tienda/m2tienda/utils/DBConnectionManager.java
@@ -1,4 +1,28 @@
 package m2tienda.m2tienda.utils;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnectionManager {
+    private static final Dotenv dotenv = Dotenv.load();
+
+    public static Connection getConnection() {
+        String url = dotenv.get("DB_URL");
+        String user = dotenv.get("DB_USER");
+        String password = dotenv.get("DB_PASSWORD");
+
+        try {
+            return DriverManager.getConnection(url, user, password);
+
+        } catch (SQLException e) {
+            System.out.println("SQL error code: " + e.getErrorCode());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("SQL error message: " + e.getMessage());
+//            e.printStackTrace();
+        }
+        return null;
+    }
 }
