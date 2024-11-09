83d179f2739de871dfef3e098b2f30ba1c7eedab
RenzoJimenezB <adrianrjb@outlook.es>
2024-11-09 17:08:28 -0500

    product entity

---
+++ src/main/java/m2tienda/m2tienda/entities/Product.java
@@ -0,0 +1,110 @@
+package m2tienda.m2tienda.entities;
+
+public class Product {
+    private Integer id;
+    private Integer categoryId;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer stock;
+    private String image_name;
+    private String image_type;
+    private Long image_size;
+    private Integer state;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getImage_name() {
+        return image_name;
+    }
+
+    public void setImage_name(String image_name) {
+        this.image_name = image_name;
+    }
+
+    public String getImage_type() {
+        return image_type;
+    }
+
+    public void setImage_type(String image_type) {
+        this.image_type = image_type;
+    }
+
+    public Long getImage_size() {
+        return image_size;
+    }
+
+    public void setImage_size(Long image_size) {
+        this.image_size = image_size;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", categoryId=" + categoryId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                ", image_name='" + image_name + '\'' +
+                ", image_type='" + image_type + '\'' +
+                ", image_size=" + image_size +
+                ", state=" + state +
+                '}';
+    }
+}
